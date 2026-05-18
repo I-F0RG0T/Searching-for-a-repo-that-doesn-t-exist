13c65b3ca474bcbf88c3a3446e2ffa9c2faed73f
I-F0RG0T <I-F0RG0T>
2026-05-19 11:35:25 +1200

    i finish mading or liek funcaillty nav bar

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route ( '/' )
-def index():
+@app.route( '/' )
+def base():
     return render_template('base.html')
+
+@app.route( '/signin' )
+def signin():
+    return render_template('signin.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
