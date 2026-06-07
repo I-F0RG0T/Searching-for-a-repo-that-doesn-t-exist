8418ba887db1c4d64fdf81e418cf9a86179f2a2b
Mica Solis <micasolis@shgcham.school.nz>
2026-06-08 11:33:12 +1200

    2 class - 8?06 | I uh, try understinf code and sqlite3 casue im a dummby :3
    Work on sign up and noting else

--- app.py
+++ app.py
@@ -1,15 +1,61 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, request,flash, session, redirect
+import sqlite3
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = "MyReallySecretKey"
+
+
+
+DATABASE = "database-1.db"
+
+def query_db(sql,args=(),one=False):
+    '''connect and query- will retun one item if one=true and can accept arguments as tuple'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    cursor.execute(sql, args)
+    results = cursor.fetchall()
+    db.commit()
+    db.close()
+    #return None if ther is no result from the query
+    #return the first item only if one=True
+    #return the list of tuples if one=False
+    return (results[0] if results else None) if one else results
+
+
+
+
+
+
 @app.route('/')
 
-def base():
+def base(): 
     return render_template('base.html')
 
-@app.route( '/signin' )
+@app.route( '/signin', methods=["GET","POST"] )
 def signin():
-    return render_template('signin.html')
+#Connect to the datavase file
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM user"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+#End of the connecting database 
+#testing sing up thing#
+    if request.method == "POST":
+        username = request.form['user']
+        password = request.form['pass']
+        #hash it with the cool secutiry function
+        hashed_password = generate_password_hash(password)
+        #write it as a new user to the database
+        sql = "INSERT INTO user (username,password) VALUES (?,?)"
+        query_db(sql,(username,hashed_password))
+        #message flashes exist in the base.html template and give user feedback
+        flash("Sign Up Successful")
+    return render_template('signin.html', results=results) 
 
 @app.route( '/post' )
 def post():
